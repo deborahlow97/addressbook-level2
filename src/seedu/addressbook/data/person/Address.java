a7887417dc67d22c56da513a559762e7a53283ce
Deborah Low Shi Lei <deborahlow97@gmail.com>
2018-02-10 21:27:55 +0800

    Edit Address class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,8 +8,9 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "123, some street, unit number, postalcode";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses has to be in the format block "
+            + "number, street name, unit number, postalcode number.";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
     public final String value;
